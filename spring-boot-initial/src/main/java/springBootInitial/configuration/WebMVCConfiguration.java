10e671d6bf7f8a909d7a65fb94374917ca7b0646
Jonatan Vicente <it@jonatanvicente.com>
2020-12-03 16:24:46 +0100

    Adding H2 DB to One-to-Many

--- spring-boot-initial/src/main/java/springBootInitial/configuration/WebMVCConfiguration.java
+++ spring-boot-initial/src/main/java/springBootInitial/configuration/WebMVCConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@ComponentScan(basePackages = {"springBootInitialDemo"})
+@ComponentScan(basePackages = {"springBootInitial"})
 @Configuration
 @EnableWebMvc
 public class WebMVCConfiguration extends WebMvcConfigurerAdapter {
---
+++ spring-boot-one-to-many/README.md
@@ -0,0 +1,3 @@
+
+### One-To-Many Example
+
