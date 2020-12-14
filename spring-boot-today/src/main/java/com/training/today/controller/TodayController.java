38d83e5257025d1954ac3d634b2e68d37888f538
Jonatan Vicente <it@jonatanvicente.com>
2020-12-14 16:16:29 +0100

    Ending config Spring Boot Today

--- spring-boot-today/src/main/java/com/training/today/controller/TodayController.java
+++ spring-boot-today/src/main/java/com/training/today/controller/TodayController.java
@@ -1,21 +1,25 @@
 package com.training.today.controller;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+
 @RestController
+@RequestMapping("/v1")
 public class TodayController {
 
-
-    public String getToday(){
-
+    @GetMapping("/today")
+    public ResponseEntity<String> getToday(){
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm:ss");
-       LocalDateTime now = LocalDateTime.now();
-       return dtf.format(now);
-
+        LocalDateTime now = LocalDateTime.now();
+        return new ResponseEntity<>(dtf.format(now), HttpStatus.OK);
     }
 
 
