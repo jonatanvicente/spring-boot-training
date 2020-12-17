39f46100997ca7e943e6d8efc76094363518ee34
Jonatan Vicente <it@jonatanvicente.com>
2020-12-17 15:55:06 +0100

    Adding Spring Cloud Gradle configs to Today Gateway

--- spring-boot-today/gateway/src/main/java/com/training/today/gateway/App.java
+++ spring-boot-today/gateway/src/main/java/com/training/today/gateway/App.java
@@ -2,11 +2,26 @@ package com.training.today.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
 @SpringBootApplication
 public class App {
 
     public static void main (String args[]){
         SpringApplication.run(App.class, args);
+    }
+
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Spring", "Training"))
+                        .uri(httpUri));
+
     }
 }
