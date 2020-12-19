5de132ae32c8dfffb46074f5dee25fa494600dca
Jonatan Vicente <it@jonatanvicente.com>
2020-12-19 16:44:26 +0100

    Reseting

--- spring-boot-security-oauth/jwt-resource-server/src/main/java/com/training/jwt/resourceserver/persistence/repository/IFooRepository.java
+++
@@ -1,8 +0,0 @@
-package com.training.jwt.resourceserver.persistence.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.training.jwt.resourceserver.persistence.model.Foo;
-
-public interface IFooRepository extends PagingAndSortingRepository<Foo, Long> {
-}
