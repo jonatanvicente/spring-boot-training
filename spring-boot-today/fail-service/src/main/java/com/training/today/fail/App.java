e893671a8284bfe481fa19a5e9f86a731b4c10a0
Jonatan Vicente <it@jonatanvicente.com>
2020-12-22 09:59:00 +0100

    More containers

--- spring-boot-today/fail-service/src/main/java/com/training/today/fail/App.java
+++ spring-boot-today/fail-service/src/main/java/com/training/today/fail/App.java
@@ -2,7 +2,9 @@ package com.training.today.fail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class App {
 
