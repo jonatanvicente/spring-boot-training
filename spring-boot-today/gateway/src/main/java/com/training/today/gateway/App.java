53a2042be2b41d9c357f8dbb7b6c2f23feff29dd
Jonatan Vicente <it@jonatanvicente.com>
2020-12-22 10:35:55 +0100

    Modifying configs

--- spring-boot-today/gateway/src/main/java/com/training/today/gateway/App.java
+++ spring-boot-today/gateway/src/main/java/com/training/today/gateway/App.java
@@ -1,16 +1,10 @@
 package com.training.today.gateway;
 
-import org.eclipse.jgit.attributes.FilterCommandRegistry;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.web.bind.annotation.RequestMapping;
-import reactor.core.publisher.Mono;
-import wiremock.org.eclipse.jetty.servlet.FilterHolder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class App {
 
